6ac66617a490ca6f3aa4b7fe9a7ec1ad2cf2d8ca
Rob Muhlestein <rob@skilstak.com>
2017-10-22 20:59:43 -0400

    save

--- parts/opening.go
+++ parts/opening.go
@@ -2,15 +2,19 @@ package parts
 
 import s "github.com/skilstak/storyeng-go"
 
+func init() {
+
 s.Parts["Start"] = s.Part{
-	ENTER: "Welcome to this story game.",
-	INPUT: "Name",
+	"ENTER": "Welcome to this story game.",
+	"INPUT": "Name",
 }
 
 s.Parts["Name"] = s.Part{
-	ENTER: "My name is Norman. What's yours?",
-	oninput: func(e) {
+	"ENTER": "My name is Norman. What's yours?",
+	"oninput": func (e s.InputEvent) {
 		s.Data["name"] = e.Line
 	},
-	LEAVE: "Nice to meet you {{.name}}.",
+	"LEAVE": "Nice to meet you {{.name}}.",
+}
+
 }
